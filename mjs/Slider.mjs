63fca6b18171563f21d00e8a27d31bf4f0cc4f4d
Saposhnikov <jean.saposhnikov@hotmail.fr>
2020-02-14 16:30:12 -0500

    images de slider

---
+++ mjs/Slider.mjs
@@ -0,0 +1,40 @@
+export class Slider {
+    constructor(elmMonSlider, config_slider){
+        this.elmMonSlider = elmMonSlider;
+        this.list_slide = config_slider.list_slide;
+        console.log('slider')
+
+    }
+
+    creerSlider() {
+
+        console.log(this.elmMonSlider.tagName)
+        console.log(this.list_slide)
+        let elmSlider = document.createElement('div')
+        elmSlider.classList.add('slider')
+        let elmSlides = document.createElement('div')
+        elmSlides.classList.add('slides')
+        let k=1
+        for (let unSlide of this.list_slide){
+            let elmSlide = document.createElement('div')
+            
+            elmSlide.id = 'slides-' + k
+            k++
+            elmSlides.appendChild(elmSlide);
+
+            let img = document.createElement('img');
+            img.src=unSlide.img
+            elmSlide.appendChild(img);
+           
+        
+            
+            //elmSlides.appendChild(elmSlide)
+            //elmSlides.appendChild(img);
+        }
+        elmSlider.appendChild(elmSlides)
+        this.elmMonSlider.appendChild(elmSlider)
+
+    }
+
+
+}
---
+++ mjs/config_slider.mjs
@@ -0,0 +1,25 @@
+export const config_slider = {
+
+    largeur : '500px',
+    hauteur : '500px',
+
+    list_slide : [
+        {
+            titre : 'Le Titre',
+            sousTitre : 'Le sous titre',
+            img : 'img/1.jpeg'
+        },
+        {
+            titre : 'Le Titre',
+            sousTitre : 'Le sous titre',
+            img : 'img/2.jpeg'
+        },
+        {
+            titre : 'Le Titre',
+            sousTitre : 'Le sous titre',
+            img : 'img/3.jpeg'
+        }
+
+
+    ]
+}
